b6d8eba274958055f1a363102ae15c5df7b9dc7e
Aleksandr <sanyakovlev@yandex.ru>
2019-04-05 11:43:51 +0700

    Implemented redirection after the battle.

--- src/main/java/ru/job4j/filters/FightingFilter.java
+++ src/main/java/ru/job4j/filters/FightingFilter.java
@@ -3,11 +3,14 @@ package ru.job4j.filters;
 import ru.job4j.DependencyContainer;
 import ru.job4j.domain.duels.ActiveDuels;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 public class FightingFilter extends HttpFilter {
@@ -27,6 +30,7 @@ public class FightingFilter extends HttpFilter {
         final String requestURI = req.getRequestURI();
         try (final ActiveDuels activeDuels = this.activeDuelsFactory.get()) {
             if (activeDuels.inDuel(userName)) {
+                session.setAttribute("fighting", true);
                 req.setAttribute("fighting", true);
                 if (requestURI.equals("/arena/duel")) {
                     chain.doFilter(req, resp);
@@ -34,8 +38,18 @@ public class FightingFilter extends HttpFilter {
                     resp.sendRedirect(req.getContextPath() + "/arena/duel");
                 }
             } else {
+                final Optional<Boolean> optFighting = Optional.ofNullable(
+                        (Boolean) session.getAttribute("fighting")
+                );
+                session.setAttribute("fighting", false);
                 if (requestURI.equals("/arena/duel")) {
-                    resp.sendRedirect(req.getContextPath() + "/arena");
+                    if (optFighting.orElse(false)) {
+                        resp.sendRedirect(
+                                req.getContextPath() + "/arena/duel/history"
+                        );
+                    } else {
+                        resp.sendRedirect(req.getContextPath() + "/arena");
+                    }
                 } else {
                     chain.doFilter(req, resp);
                 }
