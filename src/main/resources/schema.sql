b0ffe27a5b78192039f682c8fb0a2f577987ad6c
Aleksandr <sanyakovlev@yandex.ru>
2019-04-09 15:31:09 +0700

    Removed unnecessary requests.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -151,25 +151,3 @@ CREATE TABLE `final_blow`
       ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
-
-INSERT INTO duels_history (duel_id, created)
-SELECT duel_id, created
-FROM active_duels
-WHERE duel_id = 4;
-
-INSERT INTO duelists_history (user_name, duel_id, health, damage)
-SELECT user_name, duel_id, health, damage
-FROM active_duelists
-WHERE duel_id = 4;
-
-INSERT INTO attack_log_history (attacker_name, time, duel_id, target_name)
-SELECT attacker_name, time, duel_id, target_name
-FROM attack_log
-WHERE duel_id = 4;
-
-DELETE FROM active_duels WHERE duel_id = 4;
-DELETE FROM active_duelists WHERE duel_id = 4;
-DELETE FROM attack_log WHERE duel_id = 4;
-
-INSERT INTO final_blow (attacker_name, duel_id, target_name)
-VALUE ('Alexander', 1, 'Alexander2');
